63f0f21da135403adeffda64c559bf06fad6fc44
Ermogenes Palacio <epalacio@prodam.sp.gov.br>
2021-04-22 11:09:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Svelte and Tools Template
+
+Contains:
+
+- [Svelte](https://svelte.dev/)
+- [Bulma](https://bulma.io/)
+  - [Svelma](https://c0bra.github.io/svelma)
+- [Font Awesome](https://fontawesome.com/)
+  - [fa-svelte](https://github.com/alphapeter/fa-svelte)
+- [date-fns](https://date-fns.org/)
+
+Based on [this template](https://github.com/manuel3108/svelte-bulma-template).
+
+## How to use
+
+```
+npx degit ermogenes/svelte-and-tools-template project-name
+cd project-name
+npm install
+npm run dev
+```
+
+## References
+
+- [node-saas issues](https://github.com/sveltejs/language-tools/blob/master/docs/preprocessors/scss-less.md)
+
---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+	// Bulma with Svelma
+	import { Button, Modal} from 'svelma';
+
+	// FontAwesome with Fa-Svelte
+	import Icon from 'fa-svelte';
+	import { faClock } from '@fortawesome/free-solid-svg-icons';
+
+	// Date-fns
+	import { formatDistance, subDays } from 'date-fns';
+	import { ptBR } from 'date-fns/locale';
+
+	export let name;
+  	let active = false;
+	let dayBeforeYesterday = formatDistance(subDays(new Date(), 2), new Date(), { addSuffix: true, locale: ptBR });
+</script>
+
+<style global lang="scss">
+	@import "main.scss";
+</style>
+
+<main>
+	<h1 class="title is-1">Hello {name}!</h1>
+
+	<p>Bulma with Svelma ▶
+		<Button on:click={() => active = !active} type="is-primary is-small">Open modal</Button>
+		<Modal bind:active={active}>
+			<p class="image is-4by3">
+				<img alt="Test" src="https://via.placeholder.com/1280x920"/>
+			</p>
+		</Modal>
+	</p>
+
+	<p>fa-svelte ▶ <Icon icon={faClock}></Icon></p>
+	<p>date-fns ▶ Anteontem foi {dayBeforeYesterday}!</p>
+</main>
